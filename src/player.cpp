43304b6e7fa9f5a07155fe60b57e825e1176b64b
GalenEmil <emil.bonevnilsson@elev.ga.ntig.se>
2025-10-21 17:01:21 +0200

    Försökte fixa sprint systemet men lyckades inte

--- src/player.cpp
+++ src/player.cpp
@@ -8,20 +8,43 @@ Player::Player(EntityManager* entityManager, Map *map, Camera *camera, float x, 
     this->Load("data/gfx/player.png");
     this->setPosition(x, y);
     this->speed = 0.00015f;;
-    this->stamina = 4;
+    this->maxStamina = 3.0f;
+    stamina = maxStamina;
 }
+
+bool isResting = false;
+float restTimer = 5.0f;
 
 void Player::Update(sf::RenderWindow* window, InputManager inputManager, int timeElapsed) {
     float speed = this->speed * timeElapsed;
     // Sprint ability
+    // Original speed is multiplied with 3 while sprinting 
+    // Player loses 1 stamina per second while sprinting
+    // Player regains 1 stamina per second while not sprinting
     float sprintMultiplier;
-    if(inputManager.IsPressed(InputManager::SprintAbility) && stamina>-1){
+
+
+    if(inputManager.IsPressed(InputManager::SprintAbility) && stamina > 0.0f && isResting == false){
         sprintMultiplier = 3.0f;
         stamina -= 1.0f/60.0f;
+    } else if(inputManager.IsPressed(InputManager::SprintAbility) && stamina <= 0.0f && isResting == false){
+        sprintMultiplier = 1.0f;
+        isResting = true;
+    } else if(isResting == true){
+        restTimer = restTimer - 1.0f;
+        if(restTimer <= 0.0f){
+            isResting = false;
+            stamina = maxStamina;
+            restTimer = 5.0f;
+        };
     } else{
         sprintMultiplier = 1.0f;
-        stamina += 1.0f/60.0f;
     }
+    // If statement so stamina doesn't surpass max stamina
+    if(stamina > maxStamina){
+        stamina = maxStamina;
+    };
+
     speed = speed * sprintMultiplier;
     // Update player velocity
     this->velocity.x = inputManager.IsPressed(InputManager::Right) * speed -
